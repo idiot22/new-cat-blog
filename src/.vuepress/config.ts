7c02ae55574b0c4e17d8af845af6adc56e2bcc0a
idiot22 <35952356+idiot22@users.noreply.github.com>
2026-04-06 21:56:23 +0800

    gg

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/new-cat-blob/",
+  base: "/new-cat-blog/",
 
   lang: "zh-CN",
   title: "博客演示",
